9505f98afdb18a4e6bee9aad91ef98f86c09c638
Phanida Thaijamnong <33056348+mayphanida@users.noreply.github.com>
2017-11-14 18:01:00 +0700

    ทำตามจารย์ในคาบ

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  int a;
+  cout << "Hello World!" << "ABC" << endl;
+  cin >> a;
+  a = a + 14;
+  cout << "เราได้รับ";
+  
+    
   return 0;
 }
