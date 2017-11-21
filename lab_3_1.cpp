e43810c2abffcf6a5c2296e83edc6df68f9b6252
Phanida Thaijamnong <phanidatha@cvc-cha.ac.th>
2017-11-21 17:37:48 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  int a;
-  cout << "Hello World!" << "ABC" << endl;
-  cin >> a;
-  a = a + 14;
-  cout << "เราได้รับ";
-  
-    
+  cout << "Fullname : "<< "Miss "<< "Phanida Thaijamnong" << endl ;
+  cout << "Nickname : "<< "May" << endl ;
+  cout << "Gender : "<< "female" << endl ;
+  cout << "Date of birth : "<< "06 april 1999" << endl ;
+  cout << "Hobby : "<<  "Play sports and sing a song" << endl ;
+  cout << "Status : "<< "Single" <<endl ;
   return 0;
 }
